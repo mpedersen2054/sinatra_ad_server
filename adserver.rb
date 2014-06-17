cf370da76aa27faeed88ced274ea53769862a7c8
Matt Pedersen <mpedersen2054@gmail.com>
2014-06-17 00:31:30 -0500

    add datamapper and Ad properties

--- adserver.rb
+++ adserver.rb
@@ -1,6 +1,31 @@
 require 'rubygems'
 require 'sinatra'
-require 'dm-core'
-require 'dm-timestamps'
+require 'data_mapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/adserver.db")
+
+class Ad
+
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :title, String
+  property :content, Text
+  property :width, Integer
+  property :height, Integer
+  property :filename, String
+  property :url, String
+  property :is_active, Boolean
+  property :created_at, DateTime
+  property :updated_at, DateTime
+  property :size, Integer
+  property :content_type, String
+
+end
+
+# create or upgrade all tables at once
+DataMapper.auto_upgrade!
+
+get '/' do
+  "HELLO WORLD!"
+end
